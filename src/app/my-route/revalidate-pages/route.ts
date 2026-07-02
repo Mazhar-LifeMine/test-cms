674ae3fb39a784724ae1341e5d79e6519d77197b
Mazhar-LifeMine <mazarali@lifemine.io>
2026-07-02 12:55:24 +0530

    feat: simplify revalidation logic by removing dynamic paths and revalidating the root layout

--- src/app/my-route/revalidate-pages/route.ts
+++ src/app/my-route/revalidate-pages/route.ts
@@ -2,15 +2,13 @@ import { revalidatePath } from 'next/cache'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(req: NextRequest) {
-  const { paths, secret } = await req.json()
+  const { secret } = await req.json()
 
   if (secret !== process.env.REVALIDATE_SECRET) {
     return NextResponse.json({ error: 'Invalid secret' }, { status: 401 })
   }
 
-  for (const path of paths) {
-    revalidatePath(path)
-  }
+  revalidatePath('/', 'layout') // revalidates everything!
 
-  return NextResponse.json({ revalidated: true, paths })
+  return NextResponse.json({ revalidated: true })
 }
