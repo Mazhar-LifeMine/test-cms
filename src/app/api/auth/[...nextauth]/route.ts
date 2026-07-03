7392cf717f4eb21e3ab06b8c2bccb8164a3bda26
Mazhar-LifeMine <mazarali@lifemine.io>
2026-07-03 16:34:57 +0530

    feat: add NextAuth authentication handler with Google provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,15 +2,28 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
 const handler = NextAuth({
+  debug: true,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: '/login',
+  },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return baseUrl
+    },
   },
 })
 
